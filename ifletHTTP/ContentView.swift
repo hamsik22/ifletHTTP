c285d8f782f0a20dbb8fdf9d8c2c223a15793445
HamSik <payback9488@gmail.com>
2024-10-15 17:55:10 +0900

    Create Project

---
+++ ifletHTTP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ifletHTTP
+//
+//  Created by 황석현 on 10/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ifletHTTP/ifletHTTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ifletHTTPApp.swift
+//  ifletHTTP
+//
+//  Created by 황석현 on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ifletHTTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
